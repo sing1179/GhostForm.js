9dfe36c06294f8ba9444e8a80e3b050c38b16672
sing1179 <143670921+sing1179@users.noreply.github.com>
2025-06-11 21:55:46 -0400

    Create FormStructure.js

---
+++ FormStructure.js
@@ -0,0 +1,11 @@
+
+class FormStructure {
+    private List<FormField> fields;
+
+    public List<FormField> getFields() {
+        return fields;
+    }
+    public void setFields(List<FormField> fields) {
+        this.fields = fields;
+    }
+}
